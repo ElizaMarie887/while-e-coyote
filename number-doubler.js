43292174b792287df94607b4cd81b002d526cea3
Denise "Eliza" Cherry-Russell <108440338+ElizaMarie887@users.noreply.github.com>
2022-09-21 11:19:18 -0500

    Assignment completed

---
+++ number-doubler.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+let number = Number(prompt("Enter a starting number (less than 100): "))
+
+let x = 0
+
+while(number <= 100){
+    if(number *= 2)
+    console.log (number)
+}
